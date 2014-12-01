9df66de854f25334161ded98d2b179813b7bf4c7
Sergio Gómez <sergio64gr@gmail.com>
2014-12-01 18:42:06 +0100

    Peste

--- Assets/Scripts/HurtSystem.cs
+++ Assets/Scripts/HurtSystem.cs
@@ -6,22 +6,32 @@ public class HurtSystem : MonoBehaviour {
 	public int damage = 5;
 	public float force = 0;
 	public bool destroyOnCollision = false;
-	
+	public bool destroyOnTrigger = false;
+
 	private DamageSystem ds;
+
+	void OnCollisionStay2D(Collision2D target) { herir (target.transform);}
+
 	
-	void OnCollisionStay2D(Collision2D target){
+	void OnTriggerStay2D(Collider2D target) { herir (target.transform);} 
+
 		
-		if (target.transform.tag == tagDamage) {
-			Debug.Log (target.transform.tag);
+		void herir (Transform target) {
+			if (target.tag == tagDamage) {
 			ds = target.transform.GetComponent<DamageSystem>();
 			if(ds){
-				Debug.Log(tagDamage + ":"+damage);
 				ds.hurt(damage);
 				if(force > 0){
-					var direction = target.transform.position - transform.position  ;
+					var direction = target.position - transform.position  ;
 					ds.rigidbody2D.AddForce(direction.normalized * force/10,ForceMode2D.Impulse);
 				}
 			}
+
+
+			if (destroyOnTrigger) {
+				Destroy(gameObject);
+		}
+
 		}
 		
 		if (destroyOnCollision) {
