31df6e6958e2e75e8fde47e2a3ae678566a590b6
Sergio Gómez <sergio64gr@gmail.com>
2014-12-04 19:39:29 +0100

    Cosass

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+	public string nombreNivel;
+
+	public void exit(){
+				Application.Quit ();
+		}
+
+		public void play(){
+			Application.LoadLevel (nombreNivel);
+
+	
+	}
+}
